e87f2ec417bb192baa84e4ad7226ef07662712da
d9nich <dan.galaiko@gmail.com>
2020-08-13 16:23:58 +0300

    Now my JSF is working.

--- src/main/java/jsf2demo/TimeBean.java
+++ src/main/java/jsf2demo/TimeBean.java
@@ -1,14 +1,13 @@
 package jsf2demo;
 
-import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
 import javax.inject.Named;
 import java.util.Date;
 
-@Named(value = "timeBean")
+@ManagedBean(name = "timeBean")
 @RequestScoped
 public class TimeBean {
-    public TimeBean() {
-    }
 
     public String getTime(){
         return new Date().toString();
